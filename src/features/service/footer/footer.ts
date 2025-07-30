37b83fa5b4f102e2be3be4b51c637afcb1895032
Azer Kazimov <azer.kazimov@yahoo.com>
2025-07-30 21:09:32 +0400

    Add react-icons dependency and update Footer and Account components

---
+++ src/features/service/footer/footer.ts
@@ -0,0 +1,134 @@
+export const footer = [
+    {
+        title: "SUPPLEMENT",
+        links: [
+            {
+                title: "Benefits",
+                href: "/benefits"
+            },
+            {
+                title: "Formula",
+                href: "/formula"
+            },
+            {
+                title: "Immunity",
+                href: "/immunity"
+            },
+            {
+                title: "Journal",
+                href: "/journal"
+            },
+            {
+                title: "Reviews",
+                href: "/reviews"
+            },
+            {
+                title: "Buy",
+                href: "/buy"
+            }
+        ]
+    },
+    {
+        title: "LYMA LASER",
+        links: [
+            {
+                title: "LYMA Laser",
+                href: "/lyma-laser"
+            },
+            {
+                title: "Active Mist",
+                href: "/active-mist"
+            },
+            {
+                title: "Priming Serum",
+                href: "/priming-serum"
+            },
+            {
+                title: "Technology",
+                href: "/technology"
+            },
+            {
+                title: "Results",
+                href: "/results"
+            },
+            {
+                title: "Journal",
+                href: "/journal"
+            },
+            {
+                title: "Reviews",
+                href: "/reviews"
+            },
+            {
+                title: "Buy",
+                href: "/buy"
+            }
+        ]
+    },
+    {
+        title: "ABOUT",
+        links: [
+            {
+                title: "About",
+                href: "/about"
+            },
+            {
+                title: "Journal",
+                href: "/journal"
+            },
+            {
+                title: "FAQs",
+                href: "/faqs"
+            },
+            {
+                title: "Contact",
+                href: "/contact"
+            },
+            {
+                title: "Terms",
+                href: "/terms"
+            },
+            {
+                title: "Privacy",
+                href: "/privacy"
+            },
+            {
+                title: "Warranty",
+                href: "/warranty"
+            }
+        ]
+    },
+    {
+        title: "ACCOUNT",
+        links: [
+            {
+                title: "Activate",
+                href: "/activate"
+            },
+            {
+                title: "My subscription",
+                href: "/my-subscription"
+            },
+            {
+                title: "Add extras",
+                href: "/add-extras"
+            },
+            {
+                title: "My orders",
+                href: "/my-orders"
+            },
+            {
+                title: "Account details",
+                href: "/account-details"
+            },
+            {
+                title: "Payment method",
+                href: "/payment-method"
+            },
+            {
+                title: "Login details",
+                href: "/login-details"
+            }
+        ]
+    },
+]
